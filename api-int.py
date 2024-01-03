711a8526dc05eddf374a949765397e650f51c764
Shelby Jueden <shelby@jueden.org>
2024-01-03 14:51:32 -0700

    Detect loaded modules

--- api-int.py
+++ api-int.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 import sys
+
 from os.path import dirname, basename, isfile, join
 import glob
+
 try:
     import obspython as obs
+    modules = glob.glob(join(dirname(__file__),"mods/", "*.py"))
+    mods = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 except ModuleNotFoundError:
     print("This script is meant to be loaded by OBS only")
+    modsb=dir()
+    from mods import *
+    modsa=dir()
+    mods=list(set(modsb) ^ set(modsa))
+    mods.remove("modsb")
     #sys.exit(1)
 
 # Production Imports
@@ -20,8 +29,6 @@ import asyncio
 
 
 print("Loaded Mods:")
-modules = glob.glob(join("mods/", "*.py"))
-mods = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 print(mods)
 #temp = mods.output_base.OUTBase()
 #temp.iprint()
