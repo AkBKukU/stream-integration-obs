4ebfa2a45f434a2d19ed0cf62e81f5717e3594b5
Shelby Jueden <shelby@jueden.org>
2024-01-03 15:49:24 -0700

    dynamic module loading fully working

--- api-int.py
+++ api-int.py
@@ -3,9 +3,11 @@ import sys
 
 from os.path import dirname, basename, isfile, join
 import glob
+import asyncio
 
 try:
     import obspython as obs
+    from mods import *
     modules = glob.glob(join(dirname(__file__),"mods/", "*.py"))
     mods = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
 except ModuleNotFoundError:
@@ -17,18 +19,12 @@ except ModuleNotFoundError:
     mods.remove("modsb")
     #sys.exit(1)
 
-# Production Imports
-#from stream.api_twitch import APItwitch
 
-# Test Imports
-#from stream.api_twitch_test import APItwitchTest as APItwitch
-#from stream.output_base import OUTBase as OUTDectalk
 
-import asyncio
 
 
 
 print("Loaded Mods:")
 print(mods)
-#temp = mods.output_base.OUTBase()
-#temp.iprint()
+temp = getattr(sys.modules[__name__],mods[mods.index("output_base")]).OUTBase()
+temp.iprint()
